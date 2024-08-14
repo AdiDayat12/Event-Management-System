ea8a248ce4f20cfaeb465a55804062d792414001
LinAn <adilinan72@gmail.com>
2024-08-15 00:43:27 +0300

    initial commit

---
+++ src/test/java/com/practice/event_management_system/EventManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.event_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
